86f48e7717ee4488a53fdb3ac79c31a080392d33
Salvador Murillo <chavitation@gmail.com>
2019-06-13 20:39:41 -0700

    added generation of random nunumbers when each crystal is clicked

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,18 +1,45 @@
+var targetNumber
+var redCrystal 
+var purpleCrystal 
+var blueCrystal 
+var greenCrystal 
+var targetScore = 0;
 
-var redCrystal = Math.ceil(Math.random() * 11);
-var purpleCrystal = Math.ceil(Math.random() * 11);
-var blueCrystal = Math.ceil(Math.random() * 11);
-var greenCrystal = Math.ceil(Math.random() * 11);
 
 
+var generateTarget = function () {
 
-var generateTarget = function  () {
-
-    var targetNumber = Math.ceil(Math.random() * 120);
+     targetNumber = Math.ceil(Math.random() * 120);
+     redCrystal = Math.ceil(Math.random() * 11);
+     purpleCrystal = Math.ceil(Math.random() * 11);
+     blueCrystal = Math.ceil(Math.random() * 11);
+     greenCrystal = Math.ceil(Math.random() * 11);
 
     $("#target").html(targetNumber);
 
 }
+
+$("#redcrystal").click(function(){
+    targetScore = targetScore + redCrystal;
+    $("#scoreNumber").html(targetScore);
+})
+
+$("#purplecrystal").click(function(){
+    targetScore = targetScore + purpleCrystal;
+    $("#scoreNumber").html(targetScore);
+})
+
+$("#bluecrystal").click(function(){
+    targetScore = targetScore + blueCrystal;
+    $("#scoreNumber").html(targetScore);
+})
+
+$("#greencrystal").click(function(){
+    targetScore = targetScore + greenCrystal;
+    $("#scoreNumber").html(targetScore);
+})
+
+
 
 generateTarget();
 
